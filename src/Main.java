73ca6082829028c0641f338e4437527f4e86ff61
Pedro Viel <pedro.viel87@icloud.com>
2026-04-14 06:51:24 -0300

    - Improve route display

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,18 +7,81 @@ public class Main {
     public static void main(String[] args) {
         Map<String, List<String>> adjacencias = new HashMap<>();
 
-        adjacencias.put("Manaus", Arrays.asList("Belém", "Fortaleza"));
-        adjacencias.put("Belém", Arrays.asList("Manaus", "Recife", "Fortaleza"));
-        adjacencias.put("Fortaleza", Arrays.asList("Manaus", "Belém", "Recife", "Salvador"));
-        adjacencias.put("Recife", Arrays.asList("Belém", "Fortaleza", "Salvador", "BH"));
-        adjacencias.put("Salvador", Arrays.asList("Fortaleza", "Recife", "BH", "Brasília"));
-        adjacencias.put("BH", Arrays.asList("Recife", "Salvador", "RJ", "Brasília", "SP"));
-        adjacencias.put("Brasília", Arrays.asList("Salvador", "BH", "SP", "Goiânia"));
-        adjacencias.put("RJ", Arrays.asList("BH", "SP"));
-        adjacencias.put("SP", Arrays.asList("BH", "Brasília", "RJ", "Curitiba", "Goiânia"));
-        adjacencias.put("Goiânia", Arrays.asList("Brasília", "SP", "Curitiba"));
-        adjacencias.put("Curitiba", Arrays.asList("SP", "Goiânia", "Porto Alegre"));
-        adjacencias.put("Porto Alegre", Arrays.asList("Curitiba"));
+        List<String> listaManaus = new ArrayList<>();
+        listaManaus.add("Belém");
+        listaManaus.add("Fortaleza");
+        adjacencias.put("Manaus", listaManaus);
+
+        List<String> listaBelem = new ArrayList<>();
+        listaBelem.add("Manaus");
+        listaBelem.add("Recife");
+        listaBelem.add("Fortaleza");
+        adjacencias.put("Belém", listaBelem);
+
+        List<String> listaFortaleza = new ArrayList<>();
+        listaFortaleza.add("Manaus");
+        listaFortaleza.add("Belém");
+        listaFortaleza.add("Recife");
+        listaFortaleza.add("Salvador");
+        adjacencias.put("Fortaleza", listaFortaleza);
+
+        List<String> listaRecife = new ArrayList<>();
+        listaRecife.add("Belém");
+        listaRecife.add("Fortaleza");
+        listaRecife.add("Salvador");
+        listaRecife.add("BH");
+        adjacencias.put("Recife", listaRecife);
+
+        List<String> listaSalvador = new ArrayList<>();
+        listaSalvador.add("Fortaleza");
+        listaSalvador.add("Recife");
+        listaSalvador.add("BH");
+        listaSalvador.add("Brasília");
+        adjacencias.put("Salvador", listaSalvador);
+
+        List<String> listaBH = new ArrayList<>();
+        listaBH.add("Recife");
+        listaBH.add("Salvador");
+        listaBH.add("RJ");
+        listaBH.add("Brasília");
+        listaBH.add("SP");
+        adjacencias.put("BH", listaBH);
+
+        List<String> listaBrasilia = new ArrayList<>();
+        listaBrasilia.add("Salvador");
+        listaBrasilia.add("BH");
+        listaBrasilia.add("SP");
+        listaBrasilia.add("Goiânia");
+        adjacencias.put("Brasília", listaBrasilia);
+
+        List<String> listaRJ = new ArrayList<>();
+        listaRJ.add("BH");
+        listaRJ.add("SP");
+        adjacencias.put("RJ", listaRJ);
+
+        List<String> listaSP = new ArrayList<>();
+        listaSP.add("BH");
+        listaSP.add("Brasília");
+        listaSP.add("RJ");
+        listaSP.add("Curitiba");
+        listaSP.add("Goiânia");
+        adjacencias.put("SP", listaSP);
+
+        List<String> listaGoiania = new ArrayList<>();
+        listaGoiania.add("Brasília");
+        listaGoiania.add("SP");
+        listaGoiania.add("Curitiba");
+        adjacencias.put("Goiânia", listaGoiania);
+
+        List<String> listaCuritiba = new ArrayList<>();
+        listaCuritiba.add("SP");
+        listaCuritiba.add("Goiânia");
+        listaCuritiba.add("Porto Alegre");
+        adjacencias.put("Curitiba", listaCuritiba);
+
+        List<String> listaPortoAlegre = new ArrayList<>();
+        listaPortoAlegre.add("Curitiba");
+        adjacencias.put("Porto Alegre", listaPortoAlegre);
 
         Grafo grafo = new Grafo(adjacencias);
 
